c9c083c90ce1a1d5069521ff260dad84b16a3c62
Alex Almeida <alex.pans@gmail.com>
2025-11-04 13:44:59 -0300

    Code cleanup
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

--- Template.Application/Exceptions/ApplicationErrorException.cs
+++ Template.Application/Exceptions/ApplicationErrorException.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Template.Contract.Common;
+﻿using Template.Contract.Common;
 
-namespace Template.Application.Exceptions
+namespace Template.Application.Exceptions;
+
+public class ApplicationErrorException : Exception
 {
-    public class ApplicationErrorException : Exception
+    public ApplicationErrorException(List<Error> errors)
     {
-        public ApplicationErrorException(List<Error> errors)
-        {
-            Errors = errors;
-        }
-
-        public List<Error>? Errors { get; set; }
+        Errors = errors;
     }
+
+    public List<Error>? Errors { get; set; }
 }
